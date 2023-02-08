efc7f8b05af98c67b1ed04a4d45ac5fe19c41d93
Thais Helena <73835477+tsilva00@users.noreply.github.com>
2023-02-08 12:06:28 -0300

    Create testObj.js

---
+++ testObj.js
@@ -0,0 +1,11 @@
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+const hatValue = testObj.hat;    
+const shirtValue = testObj.shirt;    
+
+console.log(hatValue);
+console.log(shirtValue);
