70cfcdd795a3fb1409d4bc34bdf10ce04470130e
Thais Helena <73835477+tsilva00@users.noreply.github.com>
2023-02-08 12:25:24 -0300

    Create playerNumber.js

---
+++ playerNumber.js
@@ -0,0 +1,12 @@
+// Setup
+
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];
+
+console.log(player)
