9d6fad0347a11c080fbe2c7780d269883700e7d1
Thais Helena <73835477+tsilva00@users.noreply.github.com>
2023-02-08 12:22:07 -0300

    Create spaceName.js

---
+++ spaceName.js
@@ -0,0 +1,13 @@
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"];
+
+// Test
+console.log(entreeValue);
+console.log(drinkValue);
