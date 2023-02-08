9945129eecfcce92dd108129777f650b3c989188
Thais Helena <73835477+tsilva00@users.noreply.github.com>
2023-02-08 12:04:42 -0300

    Create myDog.js

---
+++ myDog.js
@@ -0,0 +1,11 @@
+const myDog = {
+  // Only change code below this line
+  name: "Rio de Janeiro",
+  legs: 4,
+  tails: 1,
+  friends: ['Caetano', 'Pirralho', 'Jorge ben']
+};
+
+myDog.friends.map(friend =>{
+  console.log("O nome do amigo da Rio é: " + friend)
+});
